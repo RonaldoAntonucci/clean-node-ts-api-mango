f3851408352b6cf505de0e7b1a7d23b351acfd12
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2021-02-09 01:45:19 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -99,4 +99,22 @@ describe('DbAddAccount UseCase', () => {
     }
     await expect(sut.add(accountData)).rejects.toThrow()
   })
+
+  it('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
