7180146299215b28dfd10c578337eb8a37838190
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2021-02-09 14:51:06 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,26 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    expect(account).toHaveProperty('name', 'any_name')
+    expect(account).toHaveProperty('email', 'any_email@mail.com')
+    expect(account).toHaveProperty('password', 'any_password')
+    expect(account).toHaveProperty('id')
+  })
+})
