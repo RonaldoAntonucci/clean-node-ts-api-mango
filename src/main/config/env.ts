e0d56fed8521b8775a776962a1b5c8224d4700b7
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2021-02-11 15:05:53 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL as string || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT as string || '3333'
+  port: process.env.PORT as string || '3333',
+  jwtSecret: process.env.JWT_SECRET as string || 'secret'
 }
